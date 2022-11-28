9dbcbf638373b30d411b238a7cf6388771f71b93
sarra-web <sarra.jridette@etudiant-enit.utm.tn>
2022-11-28 13:48:06 +0100

    buildf

--- api.py
+++ api.py
@@ -1,10 +1,13 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+
 load_dotenv()
-API_Key = os.getenv("API_KEY")
+print("-----")
+#print(os.getenv("PATH"))
+API_KEY=os.getenv('API_KEY')
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
@@ -15,18 +18,18 @@ parameters = {
 }
 headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': API_Key,
+     'X-CMC_PRO_API_KEY':API_KEY,
 }
 
 session = Session()
 session.headers.update(headers)
 
 try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-   # print(data)
-    for i in data['data']:
-        print("id "+str(i['id'])+"\n")
-        print("quote" + str(i['quote'])+"\n")
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+ # print(data)
+  for i in data['data']:
+    print("id "+str(i['id'])+"\n")
+    print("quote"+ str(i['quote'])+"\n")
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+  print(e)
